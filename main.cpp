ccd83560678e977f7405462298583e4eda81527f
Daniel Henrique Ferreira Carvalho <danielhfc01@gmail.com>
2025-05-02 14:33:21 -0300

    Add method to remove from list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,6 +26,22 @@ public:
     void addItem(const string &name, float price, int priority, string details, string url)
     {
         itens.emplace_back(name, price, priority, details, url);
+    }
+
+    void removeItem(int priority)
+    {
+        auto it = std::find_if(itens.begin(), itens.end(), [&priority](const Item &item)
+                               { return item.priority == priority; });
+
+        if (it != itens.end())
+        {
+            itens.erase(it);
+            cout << "Item '" << priority << "' removido da lista.\n";
+        }
+        else
+        {
+            cout << "Item '" << priority << "' nao encontrado.\n";
+        }
     }
 
     void showList()
@@ -66,7 +83,7 @@ int main()
     string details;
     string url;
 
-    int option = 1;
+    int option = 1, deletePos;
 
     while (option != 0)
     {
@@ -94,6 +111,12 @@ int main()
 
             buyList.addItem(name, price, priority, details, url);
             break;
+
+        case 4:
+            cout << "Digite a posicao do item o qual deseja remover" << endl;
+            cin >> deletePos;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            buyList.removeItem(deletePos);
 
         default:
             break;
